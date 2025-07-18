d6cc01847866433a5d6c4c0d1dc45647717e2209
Edwin WB Li <a15277019572@aliyun.com>
2025-07-18 15:28:52 +0800

    feat(nuxt): 集成 Umami 跟踪并添加相关页面
    - 在 nuxt.config.ts 中启用 nuxt-umami 模块
    - 添加 Umami 配置信息
    - 新增 umami/index.vue 页面用于测试跟踪功能
    - 在测试页面中添加按钮和事件处理函数，用于触发 Umami 事件跟踪

---
+++ src/pages/umami/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<button @click="umTrackEvent('button-1')">Button 1</button>
+		<button @click="onAction">Button 2</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+function onAction() {
+	umIdentify({ campaign: 'twix-trial' })
+	umTrackEvent('signup', { prop: 123, otherProp: 'anything', isBool: true })
+}
+
+onMounted(() => {
+	umTrackView()
+})
+</script>
